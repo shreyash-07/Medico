4bef028db1cb4dd9a139fa53250614245c917e27
shreyash-07 <shreyashanindya07@gmail.com>
2025-03-20 14:16:35 +0530

    added responsiveness

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -6,6 +6,7 @@ const Doctors = () => {
 
   const { speciality } = useParams()
   const [filterDoc,setFilterDoc] = useState([])
+  const [showFilter, setShowFilter] = useState(false)
   const navigate = useNavigate()
 
   const {doctors} = useContext(AppContext)
@@ -27,7 +28,11 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary' : ''}`}
+         onClick={()=>setShowFilter(prev => !prev)}>
+          Filters
+        </button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
           <p onClick={()=> speciality=== 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')}
            className={`ww-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer
             ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ''}`}
