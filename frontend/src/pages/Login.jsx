8fff610ca6b075e3b7e629f72fdf021a2e3dff6f
shreyash-07 <shreyashanindya07@gmail.com>
2025-03-28 01:20:03 +0530

    added the file structure for backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,19 +23,19 @@ const Login = () => {
           <div className='w-full'>
           <p>Full Name</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1'
-           type="text" onChange={(e)=>setName(e.target.name)} value={name} required />
+           type="text" onChange={(e)=>setName(e.target.value)} value={name} required />
         </div>
         }
         
         <div className='w-full'>
           <p>Email</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1'
-           type="email" onChange={(e)=>setEmail(e.target.name)} value={email} required />
+           type="email" onChange={(e)=>setEmail(e.target.value)} value={email} required />
         </div>
         <div className='w-full'>
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1'
-           type="password" onChange={(e)=>setPassword(e.target.name)} value={password} required />
+           type="password" onChange={(e)=>setPassword(e.target.value)} value={password} required />
         </div>
         <button className='bg-primary text-white w-full py-2 rounded-md text-base'>
           {state === 'Sign Up' ? 'Create Account' : 'Login' }
